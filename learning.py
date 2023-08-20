702a985f93d53df06483ba42a29039b6db5272d1
asaabaC <asaabacharlotte@gmail.com>
2023-08-20 16:40:02 -0700

    first commit

---
+++ learning.py
@@ -0,0 +1,10 @@
+a = 6
+b = 6
+answer = a+b
+
+if a>b:
+    print("the greater number is: ", a)
+elif a==b:
+    print("Yamawe thwey are all equal");
+else:
+    print("the lesser number is: ", b)
---
+++ test1.py
@@ -0,0 +1,12 @@
+a = 7
+b = 6
+c = (a+b)
+
+d = 3
+e = 2
+product = d*e
+
+if c> product:
+    print("the answer is ", c)
+else:
+    print(" the answer is",product)    
