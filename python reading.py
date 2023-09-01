9fb914b88db903bcab902869e9327c1fea3843ed
asaabaC <asaabacharlotte@gmail.com>
2023-08-31 22:44:04 -0700

    revision

--- python reading.py
+++ python reading.py
@@ -16,3 +16,26 @@ print (type(x))
 print (type(y))
 
  
+x, y, z, = "orange", "banana", "cherry"
+print(x)
+print(y)
+print(z)
+
+fruits = ( "orange", "banana", "cherry" )
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+x = "python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = "awesome"
+defmyfunc ()
+x = "fantastic"
+print("python is" + x)
+
+
+
